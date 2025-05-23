f42d52a86d5123e97b484f3fa916f1e211d583da
yizhe <liaoyizhe75@gmail.com>
2025-05-23 23:43:36 +0800

    fix: implement rewrites for backend API proxying in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `${process.env.BACKEND_API}/:path*`, // Proxy to Backend
+      }
+    ]
+  }
 };
 
 export default nextConfig;
